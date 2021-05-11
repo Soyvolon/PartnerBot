03ccfeeb63406432e3fa6c7aeae23c6fe3d3d147
Andrew Bounds <andybounds@gmail.com>
2021-05-11 10:10:33 -0400

    Added a reboot command for cessum admins.

---
+++ PartnerBot.Discord/Commands/Admin/RebootCommand.cs
@@ -0,0 +1,34 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using PartnerBot.Discord.Commands.Conditions;
+using System.Threading.Tasks;
+
+namespace PartnerBot.Discord.Commands.Admin
+{
+    public class RebootCommand : CommandModule
+    {
+        private readonly DiscordShardedClient _client;
+        public RebootCommand(DiscordShardedClient client)
+        {
+            _client = client;
+        }
+
+        [Command("reboot")]
+        [Description("Reboots the selected shard.")]
+        [RequireCessumAdmin]
+        public async Task RebootCommandAsync(CommandContext ctx, [Description("Shard to reboot")] int shard)
+        {
+            if(_client.ShardClients.TryGetValue(shard, out var client))
+            {
+                await client.DisconnectAsync();
+                await client.ConnectAsync();
+                await RespondSuccess($"Shard {shard} restarted.");
+            }
+            else
+            {
+                await RespondError($"Unable to get a shard by the ID of {shard}");
+            }
+        }
+    }
+}
